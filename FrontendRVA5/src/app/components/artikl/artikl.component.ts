a3fbeb136e8a60ec3eb266ce05841b56d7eb1e02
username** <marko.bojkic@uns.ac.rs>
2020-05-12 17:01:20 +0200

    Vezbe 9

--- FrontendRVA5/src/app/components/artikl/artikl.component.ts
+++ FrontendRVA5/src/app/components/artikl/artikl.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ArtiklService } from './../../services/artikl.service';
+import { Artikl } from './../../models/artikl';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-artikl',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtiklComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['id', 'naziv', 'proizvodjac', 'actions'];
+  dataSource: MatTableDataSource<Artikl>;
+
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  constructor(private artiklService: ArtiklService) { }
 
   ngOnInit(): void {
+    console.log('Inicijalizacija Artikl komponente!');
+    this.loadData();
+  }
+
+  public loadData() {
+    this.artiklService.getAllArtikl().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); //   MoJa    -->MoJa
+    filterValue = filterValue.toLocaleLowerCase(); // MoJa --> moja
+    this.dataSource.filter = filterValue;
   }
 
 }
